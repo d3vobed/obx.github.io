a23291075baa1ca1e821a3d5e740a68050ca5966
Abang Obed <66479041+d3vobed@users.noreply.github.com>
2025-02-21 01:09:35 +0100

    Update whoami.jsx

--- app/routes/whoami/whoami.jsx
+++ app/routes/whoami/whoami.jsx
@@ -25,38 +25,52 @@ const WhoamiText = ({ visible, titleId }) => (
     </Heading>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
    
-Abang 'obx' Obed is a Mobile Developer and Security Researcher at <Link href="https://www.gigafro.com">Gigafro</Link>. His work spans from software exploit development, hardware prototyping, developer operations and 3d composting. Currently pursuing a B.Tech in Cyber-Security Science, Abang has foundation in computer science, physiology, reverse engineering, chemistry, and cryptography.
+Abang 'obx' Obed is a Part-time Mobile Developer and Security Researcher at <Link href="https://www.gigafro.com">Gigafro</Link>. His work spans from software exploit & development, hardware prototyping, developer operations and Video composting & Visual Effects. Currently pursuing a B.Tech in Cyber-Security Science, Abang has foundation in computer science, physiology, reverse engineering, chemistry, and cryptography.
 
 </Text>
 <Text className={styles.description} data-visible={visible} size="l" as="p">
-Abang's contributions to computer research include discovering software vulnerabilities, learning to bypass authorizated networked systems, building softwares and studying NSA code-breaker archives for advanced cryptographic concepts. 
+Abang's contributions to computer research include discovering software vulnerabilities, learning to bypass networked systems, building softwares and studying NSA code-breaker archives for advanced cryptographic concepts. 
 
 </Text>   
 <Text className={styles.description} data-visible={visible} size="l" as="p">
   <em><b>Notable achievements include:</b></em>
   <ul>
     <li>Developing a <strong>Mobile C2 Framework</strong>.</li>
+
     <li>Created a <strong>Load Balancer C++ with gRPC</strong>.</li>
+
     <li>Built a <strong>VM hypervisor using the QEMU library</strong>.</li>
-    <li>Developed a <strong>Chromium service worker extension in Node.js</strong>.</li>
+
+    <li><strong>Chromium service worker extension</strong> in Node.js.</li>
+
     <li>Simulated <strong> Matrix Computation with my AMD radeon DDR3</strong>.</li>
-    <li>AI Model Deployment on Edge Devices using<strong> Cloudfare</strong>.</li>
+
+    <li>Model Deployment on Edge Devices using<strong> Cloudfare workers & zero trust</strong>.</li>
+
     <li>Delivered <strong>six production-grade websites</strong>, an app and multiple SDKs for businesses.</li>
+
     <li>Big Data ETL Pipeline for <strong>User Behavior Analysis on soccer games</strong>.</li>
+
     <li>Blockchain powered Dapps for tracking <strong>Agricultural </strong>Items through an inventory.</li>
+
     <li>Developing a <strong>3D Compositing & storyboard</strong> pipeline for my short film.</li>
+
     <li>Built a <strong>video stream </strong>server with <strong>rxJs,threejs & tailwind css</strong>.</li>
+
     <li>Contributed to the setup of a <strong> quadcopter drone simulink model.</strong>.</li>
+
     <li>Writing automation scripts for <strong>Blender, Houdini, After Effects, godot, and Unity</strong>.</li>
+
     <li>Assisted in creating a driving simulation game with <strong>C#</strong>.</li>
-    <li>Authorization bypass on CodeSandbox<strong>(Traiged)</strong>, IDOR on Coinhako<strong>(Needs more Info) .</strong> </li>
+
+    <li>Authorization bypass on CodeSandbox<strong>(Traiged)</strong </li>
 
   </ul>
   <Link href="/articles">Find out more(pls)</Link>
 </Text>
 
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-    In his spare time, Abang explores security researching, drone piloting, Windows internals, FC25 and unreal engine development. He also enjoys creating short films, hacking through CTFs, and researching AGI and machine learning.
+    In his spare time, Abang explores security researching, Windows internals, FC25 and 3d development. He also enjoys creating short films, hacking through CTFs, and researching about machine learning & IOS exploits.
     {' '}
     Feel free to reach out for collaborations or discussions @ <strong>hitme@obx0x3.tech</strong>
     </Text>
